b661362aec891c9de007cf6a4756cb83651f172b
Gal Jeza <gal.jeza55@gmail.com>
2026-03-12 16:48:52 +0100

    nvim, thunar

--- home.nix
+++ home.nix
@@ -1,23 +1,153 @@
 { config, pkgs, ... }:
 {
-  # this must match your username
-  home.username = "galjeza";
+  # this must match your username home.username = "galjeza";
   home.homeDirectory = "/home/galjeza";
 
   # this must match system.stateVersion in configuration.nix
   home.stateVersion = "25.11";
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "JetBrainsMono Nerd Font" ];
+    };
+  };
 
   # let home-manager manage itself
   programs.home-manager.enable = true;
 
- programs.git = {
-  enable = true;
-  settings = {
-    user.name = "Gal Jeza";
-    user.email = "gal.jeza55@gmail.com";
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Gal Jeza";
+      user.email = "gal.jeza55@gmail.com";
+    };
   };
-};
-programs.neovim = {
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "JetBrainsMono Nerd Font:size=11";
+      };
+    };
+  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      left = "h";
+      down = "j";
+      up = "k";
+      right = "l";
+      terminal = "foot";
+      menu = "wmenu-run";
+
+      output = {
+        "*".bg =
+          "${pkgs.nixos-artwork.wallpapers.binary-red}/share/wallpapers/binary-red-2024-02-15/contents/images/nix-wallpaper-binary-red.png fill";
+        "Virtual-1".mode = "1920x1080@60Hz";
+      };
+
+      # keybindings
+      keybindings =
+        let
+          mod = modifier;
+        in
+        {
+          "${mod}+Return" = "exec ${terminal}";
+          "${mod}+Shift+q" = "kill";
+          "${mod}+d" = "exec ${menu}";
+          "${mod}+Shift+c" = "reload";
+          "${mod}+Shift+e" = "exec swaynag -t warning -m 'Exit sway?' -B 'Yes' 'swaymsg exit'";
+
+          "${mod}+${left}" = "focus left";
+          "${mod}+${down}" = "focus down";
+          "${mod}+${up}" = "focus up";
+          "${mod}+${right}" = "focus right";
+          "${mod}+Left" = "focus left";
+          "${mod}+Down" = "focus down";
+          "${mod}+Up" = "focus up";
+          "${mod}+Right" = "focus right";
+
+          "${mod}+Shift+${left}" = "move left";
+          "${mod}+Shift+${down}" = "move down";
+          "${mod}+Shift+${up}" = "move up";
+          "${mod}+Shift+${right}" = "move right";
+          "${mod}+Shift+Left" = "move left";
+          "${mod}+Shift+Down" = "move down";
+          "${mod}+Shift+Up" = "move up";
+          "${mod}+Shift+Right" = "move right";
+
+          "${mod}+1" = "workspace number 1";
+          "${mod}+2" = "workspace number 2";
+          "${mod}+3" = "workspace number 3";
+          "${mod}+4" = "workspace number 4";
+          "${mod}+5" = "workspace number 5";
+          "${mod}+6" = "workspace number 6";
+          "${mod}+7" = "workspace number 7";
+          "${mod}+8" = "workspace number 8";
+          "${mod}+9" = "workspace number 9";
+          "${mod}+0" = "workspace number 10";
+
+          "${mod}+Shift+1" = "move container to workspace number 1";
+          "${mod}+Shift+2" = "move container to workspace number 2";
+          "${mod}+Shift+3" = "move container to workspace number 3";
+          "${mod}+Shift+4" = "move container to workspace number 4";
+          "${mod}+Shift+5" = "move container to workspace number 5";
+          "${mod}+Shift+6" = "move container to workspace number 6";
+          "${mod}+Shift+7" = "move container to workspace number 7";
+          "${mod}+Shift+8" = "move container to workspace number 8";
+          "${mod}+Shift+9" = "move container to workspace number 9";
+          "${mod}+Shift+0" = "move container to workspace number 10";
+
+          "${mod}+b" = "splith";
+          "${mod}+v" = "splitv";
+          "${mod}+s" = "layout stacking";
+          "${mod}+w" = "layout tabbed";
+          "${mod}+e" = "layout toggle split";
+          "${mod}+f" = "fullscreen";
+          "${mod}+Shift+space" = "floating toggle";
+          "${mod}+space" = "focus mode_toggle";
+          "${mod}+a" = "focus parent";
+          "${mod}+Shift+minus" = "move scratchpad";
+          "${mod}+minus" = "scratchpad show";
+          "${mod}+r" = "mode resize";
+        };
+
+      modes = {
+        resize = {
+          "${left}" = "resize shrink width 10px";
+          "${down}" = "resize grow height 10px";
+          "${up}" = "resize shrink height 10px";
+          "${right}" = "resize grow width 10px";
+          "Left" = "resize shrink width 10px";
+          "Down" = "resize grow height 10px";
+          "Up" = "resize shrink height 10px";
+          "Right" = "resize grow width 10px";
+          "Return" = "mode default";
+          "Escape" = "mode default";
+        };
+      };
+
+      bars = [
+        {
+          position = "top";
+          statusCommand = "while date +'%Y-%m-%d %X'; do sleep 1; done";
+          colors = {
+            statusline = "#ffffff";
+            background = "#323232";
+            inactiveWorkspace = {
+              background = "#32323200";
+              border = "#32323200";
+              text = "#5c5c5c";
+            };
+          };
+        }
+      ];
+    };
+  };
+
+  programs.neovim = {
     enable = true;
     defaultEditor = true; # sets $EDITOR to nvim
     viAlias = true; # lets you type 'vi' to open nvim
@@ -82,11 +212,13 @@ programs.neovim = {
     firefox
     zellij
     waybar
-    wofi
     wdisplays
     zoxide
-    nixfmt-rfc-style # formatting for nix files until i have neovim configured
     lazygit
+    nixfmt-rfc-style # formatting for nix files until i have neovim configured
+    nodejs
+    nerd-fonts.jetbrains-mono
+    xfce.thunar # gui file manager
   ];
 
 }
