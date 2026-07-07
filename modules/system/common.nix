7e1eeb25d18a5d374b1631c2ba3a5746a5a6bb8f
Gal Jeza <gal.jeza55@gmail.com>
2026-07-07 08:47:43 +0200

    remove flatpak

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -261,7 +261,6 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
-  services.flatpak.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
